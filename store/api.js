1b13e9dfa02bb560de8d1db15b087e43d79eca22
Logan Gouget <logan.gouget@gmail.com>
2018-11-13 21:43:59 +0100

    Auto stash before merge of "master" and "origin/master"

--- store/api.js
+++ store/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosi = axios.create({
-    baseURL: 'http://192.168.1.65:3000/api',
+    baseURL: 'http://localhost:3000/api',
     timeout: 5000,
     headers: {'Content-Type': 'application/json'}
 });
